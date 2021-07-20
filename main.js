b0d6801da251af475f59d7b24796d8f70b610bfb
ItsYuvan <86912444+ItsYuvan@users.noreply.github.com>
2021-07-20 17:23:38 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var Song=""
+function preload(){
+Song = loadSound("Magic.mp3")
+}
+
+function setup(){
+    canvas = createCanvas(600,500)
+    canvas.center();
+
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
